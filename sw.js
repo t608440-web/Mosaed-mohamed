88da48cf8d504a307b9c97bcff4c15f8f92ab8d6
t608440-web <t608440@gmail.com>
2026-08-23 17:28:00 +0300

    مساعد محمد

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='mosaed-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
