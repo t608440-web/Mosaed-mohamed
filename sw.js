3eae32b9a46a6374408690c7875f9a18672be860
t608440-web <t608440@gmail.com>
2026-08-23 17:05:41 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='mosaed-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
